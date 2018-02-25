253fcdc996c26ebd263996fd4340bd93b32cc8e8
rajkundu <rjk368@gmail.com>
2018-02-25 00:01:49 -0500

    Altered LCD UI behavior

--- Include/LCD.h
+++ Include/LCD.h
@@ -1,19 +1,26 @@
 /*--------------------------------------------------//--------------------------------------------------//
 
-Team 9502, Liberty High School VEX Robotics Team
-File: LCD.h
-Description: Contains code for LCD Display
+Team 9502, Liberty High School VEX Robotics
+File: ../Includes/LCD2.0.h
+Description: Contains LCD Code
 
 //--------------------------------------------------//--------------------------------------------------*/
+
+#include "../Include/A_autonomous_routines.h"
+
+int refreshRate;
+string selectString;
+short routineNum = 0;
 
 //Array of autonomous routine names
 string routineNames[] =
 {
-	"   Do Nothing   "
+	"   Do Nothing   ",
+	"Mobile Goal  (R)",
+	"  TEST PROGRAM  "
 };
 
-//"Select" string to be shown on bottom line of LCD
-string selectString;
+int numRoutines = sizeof(routineNames)/sizeof(routineNames[0]);
 
 //--------------------------------------------------//--------------------------------------------------//
 
@@ -27,7 +34,7 @@ void setSelectString()
 		selectString = ("     Select    >");
 	}
 	//Otherwise, if on the last, rightmost routine...
-	else if(routineNum == (sizeof(routineNames) - 1))
+	else if(routineNum == (numRoutines - 1))
 	{
 		//Hide the right arrow
 		selectString = ("<    Select     ");
@@ -40,64 +47,34 @@ void setSelectString()
 	}
 }
 
-//--------------------------------------------------//--------------------------------------------------//
-
-void flashOnLCD(string *textTopLine, string *textBottomLine, int time, short numTimes)
+void setLCDRefreshRate(int inputRefreshRate)
 {
-	//Run a loop that runs (numTimes) times
-	for(short i = 0; i < numTimes; i++)
-	{
-		//Only clear the top line if (textTopLine) contains text
-		if(*textTopLine != "\0")
-		{
-			clearLCDLine(0);
-		}
-		//Only clear the bottom line if (textBottomLine) contains text
-		if(*textBottomLine != "\0")
-		{
-			clearLCDLine(1);
-		}
-		//Wait (time) milliseconds before showing the text
-		wait1Msec(time);
-		//Only display (textTopLine) on the top line if it contains text
-		if(*textTopLine != "\0")
-		{
-			displayLCDCenteredString(0, *textTopLine);
-		}
-		//Only display (textBottomLine) on the bottom line if it contains text
-		if(*textBottomLine != "\0")
-		{
-			displayLCDCenteredString(1, *textBottomLine);
-		}
-		//Wait (time) milliseconds before hiding the text
-		wait1Msec(time);
-	}
+	refreshRate = inputRefreshRate;
+	return;
 }
 
 task autonomousSelector()
 {
-	bool autonomousSelected = false;
-
-	// Turn on the LCD backlight, signifying that it needs input
+		// Turn on the LCD backlight, signifying that it needs input
 	bLCDBacklight = true;
 
-	// While the robot is disabled and the autonomous routine has not yet been selected...
-	// (Wouldn't want the robot to be stuck waiting for a program selection the whole match!)
-	while((bIfiRobotDisabled) && (!autonomousSelected))
+	// // While the robot is disabled...
+	// // (Wouldn't want the robot to be stuck waiting for a program selection the whole match!)
+	while(!bIfiRobotDisabled)
 	{
 		// Decide which arrows to show: left, right or both?
 		setSelectString();
 
 		//Display current autonomous routine name on the top line
 		displayLCDCenteredString(0, routineNames[routineNum]);
-		//Display prompt to select on the bottom line
+		//Display "Select" on the bottom line
 		displayLCDCenteredString(1, selectString);
 
 		//If (the left LCD button is pressed) and (the currently displayed routine is not the first)...
-		if((nLCDButtons == 1) && (routineNum > 0))
+		if(nLCDButtons == 1 && (routineNum > 0))
 		{
 			//Wait for button release
-			while((nLCDButtons == 4) || (vexRT[Btn5U] == 1))
+			while(nLCDButtons == 1)
 			{
 				//Do nothing
 				EndTimeSlice();
@@ -107,10 +84,10 @@ task autonomousSelector()
 		}
 
 		//Otherwise, if (the right LCD button is pressed) and (the currently displayed routine is not the last)...
-		else if((nLCDButtons == 4) && (routineNum < (sizeof(routineNames) - 1)))
+		else if(nLCDButtons == 4 && (routineNum < (numRoutines - 1)))
 		{
 			//Wait for button release
-			while((nLCDButtons == 4) || (vexRT[Btn6U] == 1))
+			while(nLCDButtons == 4)
 			{
 				//Do nothing
 				EndTimeSlice();
@@ -120,12 +97,9 @@ task autonomousSelector()
 		}
 
 		//Otherwise, if the center LCD button is pressed...
-		else if((nLCDButtons == 2))
+		else if(nLCDButtons == 2)
 		{
-			// Notify that program has been selected
-			displayLCDString(0, 0, "Routine Selected");
-			// Break the loop
-			autonomousSelected = true;
+			break;
 		}
 
 		//Else, if no LCD buttons are pressed...
@@ -134,9 +108,12 @@ task autonomousSelector()
 			//Do nothing
 			EndTimeSlice();
 		}
+
+		//Refresh rate
+		wait1Msec(1000.0/refreshRate);
 	}
 
-	//Turn off the LCD backlight, signifying that it does not need any input
+	displayLCDCenteredString(1, " ^  Selected  ^ ");
 	bLCDBacklight = false;
 
 	//End this task
